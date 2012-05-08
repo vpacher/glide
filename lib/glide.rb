0582fcb661fd747f06d1dd9a0140a3a33e559e4f
Volker Pacher <volker.pacher@gmail.com>
2012-05-08 12:15:01 +0100

    changed to classs

--- lib/glide.rb
+++ lib/glide.rb
@@ -4,40 +4,41 @@ require 'httparty'
 require 'json'
 
 module Glide
+		@@api_key = ""
 
-	@@api_key = ""
-
-	def self.api_key
-		@@api_key
-	end
-
-	def self.api_key=(key)
-		@@api_key = key
-	end
-
-	def self.get_qote(services = ['elec'], extra = {}, period = 6, tenants = 1)
-		quotes = {}
-		services.each do |service|
-			quotes[service] = get_service_quote(service, extra[service], period, tenants)
+		def self.api_key
+			@@api_key
 		end
-		calculate_totals(quotes)
-	end
 
-	def self.get_service_quote(service, extra, period, tenants)
-		res = Timeout::timeout(5) { HTTParty.get("https://api.glide.uk.com/signup/servicePrice.json", :query => {:service => service, :period => period, :extra => extra, :tenants => tenants, :key => api_key}) }
-		JSON.parse(res.body)["results"]
-	rescue Timeout::Error => e
-		{"message" => "A timeout error has occured", "error" => 2}
-	rescue
-		{"message" => "An error has occured", "error" => 3}
-	end
+		def self.api_key=(key)
+			@@api_key = key
+		end
+	class << self
 
-	def self.calculate_totals(quotes)
-		quotes["total"] = {}
-		quotes["total"]["tenant_week"] = quotes.map { |e| e[1]["tenant_week"].to_f }.reduce(:+).to_s
-		quotes["total"]["tenant_month"] = quotes.map { |e| e[1]["tenant_month"].to_f }.reduce(:+).to_s
-		quotes["total"]["monthly_fee"] = quotes.map { |e| e[1]["monthly_fee"].to_f }.reduce(:+).to_s
-		
-		quotes		
+		def get_qote(services = ['elec'], extra = {}, period = 6, tenants = 1)
+			quotes = {}
+			services.each do |service|
+				quotes[service] = get_service_quote(service, extra[service], period, tenants)
+			end
+			calculate_totals(quotes)
+		end
+
+		def get_service_quote(service, extra, period, tenants)
+			res = Timeout::timeout(5) { HTTParty.get("https://api.glide.uk.com/signup/servicePrice.json", :query => {:service => service, :period => period, :extra => extra, :tenants => tenants, :key => api_key}) }
+			JSON.parse(res.body)["results"]
+		rescue Timeout::Error => e
+			{"message" => "A timeout error has occured", "error" => 2}
+		rescue
+			{"message" => "An error has occured", "error" => 3}
+		end
+
+		def calculate_totals(quotes)
+			quotes["total"] = {}
+			quotes["total"]["tenant_week"] = quotes.map { |e| e[1]["tenant_week"].to_f }.reduce(:+).to_s
+			quotes["total"]["tenant_month"] = quotes.map { |e| e[1]["tenant_month"].to_f }.reduce(:+).to_s
+			quotes["total"]["monthly_fee"] = quotes.map { |e| e[1]["monthly_fee"].to_f }.reduce(:+).to_s
+
+			quotes		
+		end
 	end
 end
